edba076aa234f75db2611b50efba80d8c31af977
Gagik Kyurkchyan <gagik@xamlab.com>
2021-01-23 13:28:57 +0400

    Initial commit

---
+++ src/App/Xamflix.App.Forms/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace Xamflix.App.Forms
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
