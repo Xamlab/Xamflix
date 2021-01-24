cb1763d69e0debe48dfbfe44c5915fa35ca86c3a
vardan <vardan@xamlab.com>
2021-01-25 00:20:28 +0400

    animation initial commit

--- src/App/Xamflix.App.Forms/MainPage.xaml.cs
+++ src/App/Xamflix.App.Forms/MainPage.xaml.cs
@@ -30,15 +30,17 @@ namespace Xamflix.App.Forms
             switch (e.State)
             {
                 case MediaPlayerState.Playing:
-                {
-                    await PosterImage.FadeTo(0);
-                    break;
-                }
+                    {
+                        await PosterImage.FadeTo(0);
+                        await ScaleDownTitleImageAsync();
+                        break;
+                    }
                 default:
-                {
-                    await PosterImage.FadeTo(1);
-                    break;
-                }
+
+                    {
+                        await PosterImage.FadeTo(1);
+                        break;
+                    }
             }
         }
 
@@ -71,7 +73,7 @@ namespace Xamflix.App.Forms
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            await Task.Delay(2000);
+            await Task.Delay(1000);
             var media = new MediaItem(
                 Uri.EscapeUriString(
                     @"https://xamflixdevgwcmedia-gewc1.streaming.media.azure.net/9f4ee191-59ff-48bf-bf53-2bfbec396d8b/The Midnight Sky  Final Trailer .ism/manifest(format=m3u8-aapl)"))
@@ -80,6 +82,57 @@ namespace Xamflix.App.Forms
             };
             TrailerVideoView.Source = media;
             Trace.WriteLine($"Setting media item  {JsonSerializer.Serialize(media)}");
+        }
+
+        private async Task ScaleDownTitleImageAsync()
+        {
+            await Task.Delay(10000);
+            var parentAnimation = new Animation();
+            
+
+            // Title Image
+            var imageAnimation1 = new Animation(v => TitleImage.WidthRequest = v, TitleImage.WidthRequest, 259, Easing.CubicOut);
+            var imageAnimation2 = new Animation(v => TitleImage.HeightRequest = v, TitleImage.HeightRequest, 189, Easing.CubicOut);
+            var imageAnimation3 = new Animation(v => TitleImage.Opacity = v, 1, 0.6, Easing.Linear);
+
+            // Title Label
+            var titleLabelAnimation1 = new Animation(v => TitleLabel.Opacity = v, 1, 0, Easing.Linear);
+            var titleLabelAnimation2 = new Animation(v => TitleLabel.TranslationY = v, TitleLabel.TranslationY, -50, Easing.Linear);
+
+            // Title Description
+            var descriptionAnimation1 = new Animation(v => DescriptionLabel.Opacity = v, 1, 0, Easing.Linear);
+            var descriptionAnimation2 = new Animation(v => DescriptionLabel.TranslationY = v, DescriptionLabel.TranslationY, -100, Easing.Linear);
+
+            //Info Container
+            var ContainerAnimation1 = new Animation(v => InfoContainer.TranslationY = v, InfoContainer.TranslationY, -130, Easing.Linear);
+
+            // Buttons
+            var buttonAnmiation1 = new Animation(v => PlayButton.Opacity = v, 1, 0.6, Easing.Linear);
+            var buttonAnmiation2 = new Animation(v => MoreButton.Opacity = v, 1, 0.6, Easing.Linear);
+
+            parentAnimation.Add(0, 1, imageAnimation1);
+            parentAnimation.Add(0, 1, imageAnimation2);
+            parentAnimation.Add(0, 1, imageAnimation3);
+
+            parentAnimation.Add(0, 0.7, titleLabelAnimation1);
+            parentAnimation.Add(0.2, 1, titleLabelAnimation2);
+
+            parentAnimation.Add(0, 0.7, descriptionAnimation1);
+            parentAnimation.Add(0.2, 1, descriptionAnimation2);
+
+            parentAnimation.Add(0.2, 1, ContainerAnimation1);
+
+            parentAnimation.Add(0, 1, buttonAnmiation1);
+            parentAnimation.Add(0, 1, buttonAnmiation2);
+
+            parentAnimation.Commit(this, "ScaleDownAnimation", 16, 1000, null, (v, c) => HideLables());
+
+        }
+
+        private void HideLables() 
+        {
+            //TitleLabel.IsVisible = false;
+            //DescriptionLabel.IsVisible = false;
         }
     }
 }
