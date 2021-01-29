68a0ffb96e5f25dd5649dc657ae91f66ef338429
Gagik Kyurkchyan <gagik@xamlab.com>
2021-01-29 11:21:54 +0400

    Minor fixes

--- src/App/Xamflix.App.Forms/MainPage.xaml.cs
+++ src/App/Xamflix.App.Forms/MainPage.xaml.cs
@@ -14,7 +14,7 @@ namespace Xamflix.App.Forms
 {
     public partial class MainPage
     {
-        private bool _collapsed;
+        private bool _collapsed = false;
 
         public MainPage()
         {
@@ -74,10 +74,87 @@ namespace Xamflix.App.Forms
         protected override async void OnAppearing()
         {
             base.OnAppearing();
-            await Task.Delay(2000);
-            var media = new MediaItem(
-                Uri.EscapeUriString(
-                    @"https://xamflixdevgwcmedia-gewc1.streaming.media.azure.net/c2c86ac8-5dbc-479d-9bad-4bcac71901d7/Pride%20&%20Prejudice.ism/manifest(format=m3u8-aapl)"))
+            await Task.Delay(1000);
+            Play();
+        }
+
+        private async Task ScaleDownTitleImageAsync()
+        {
+            await Task.Delay(7000);
+            var parentAnimation = new Animation();
+
+            // Title Image
+            var imageAnimation1 = new Animation(v => TitleImage.Scale = v, 1, 0.7, Easing.CubicOut);
+            var imageAnimation2 = new Animation(v => TitleImage.Opacity = v, 1, 0.5, Easing.Linear);
+
+            // Title Label
+            var titleLabelAnimation1 = new Animation(v => TitleLabel.Opacity = v, 1, 0, Easing.Linear);
+            var titleLabelAnimation2 = new Animation(v => TitleLabel.TranslationY = v, TitleLabel.TranslationY, 60, Easing.Linear);
+
+            // Title Description
+            var descriptionAnimation1 = new Animation(v => DescriptionLabel.Opacity = v, 1, 0, Easing.Linear);
+            var descriptionAnimation2 = new Animation(v => DescriptionLabel.TranslationY = v, DescriptionLabel.TranslationY, 60, Easing.Linear);
+
+            // Buttons
+            var buttonAnimation1 = new Animation(v => PlayButton.Opacity = v, 1, 0.6, Easing.Linear);
+            var buttonAnimation2 = new Animation(v => MoreButton.Opacity = v, 1, 0.6, Easing.Linear);
+
+            parentAnimation.Add(0.1, 1, imageAnimation1);
+            parentAnimation.Add(0, 1, imageAnimation2);
+
+            parentAnimation.Add(0, 0.3, titleLabelAnimation1);
+            parentAnimation.Add(0, 0.5, titleLabelAnimation2);
+
+            parentAnimation.Add(0, 0.3, descriptionAnimation1);
+            parentAnimation.Add(0, 0.5, descriptionAnimation2);
+
+            parentAnimation.Add(0, 1, buttonAnimation1);
+            parentAnimation.Add(0, 1, buttonAnimation2);
+
+            parentAnimation.Commit(this, "ScaleDownAnimation", 16, 1000, null, null);
+            _collapsed = true;
+        }
+
+        private async Task ScaleOutTitleImageAsync()
+        {
+            await Task.Delay(10);
+            var parentAnimation = new Animation();
+
+            // Title Image
+            var imageAnimation1 = new Animation(v => TitleImage.Scale = v, 0.7, 1, Easing.CubicOut);
+            var imageAnimation2 = new Animation(v => TitleImage.Opacity = v, 0.5, 1, Easing.Linear);
+
+            // Title Label
+            var titleLabelAnimation1 = new Animation(v => TitleLabel.Opacity = v, 0, 1, Easing.Linear);
+            var titleLabelAnimation2 = new Animation(v => TitleLabel.TranslationY = v, TitleLabel.TranslationY, -5, Easing.Linear);
+
+            // Title Description
+            var descriptionAnimation1 = new Animation(v => DescriptionLabel.Opacity = v, 0, 1, Easing.Linear);
+            var descriptionAnimation2 = new Animation(v => DescriptionLabel.TranslationY = v, DescriptionLabel.TranslationY, -5, Easing.Linear);
+
+            // Buttons
+            var buttonAnimation1 = new Animation(v => PlayButton.Opacity = v, 0.6, 1, Easing.Linear);
+            var buttonAnimation2 = new Animation(v => MoreButton.Opacity = v, 0.6, 1, Easing.Linear);
+
+            parentAnimation.Add(0.1, 1, imageAnimation1);
+            parentAnimation.Add(0, 1, imageAnimation2);
+
+            parentAnimation.Add(0, 0.3, titleLabelAnimation1);
+            parentAnimation.Add(0, 0.5, titleLabelAnimation2);
+
+            parentAnimation.Add(0, 0.3, descriptionAnimation1);
+            parentAnimation.Add(0, 0.5, descriptionAnimation2);
+
+            parentAnimation.Add(0, 1, buttonAnimation1);
+            parentAnimation.Add(0, 1, buttonAnimation2);
+
+            parentAnimation.Commit(this, "ScaleOutAnimation", 16, 1000, null, null);
+            _collapsed = false;
+        }
+
+        private async Task Animate()
+        {
+            if(_collapsed)
             {
                 await ScaleOutTitleImageAsync();
             }
@@ -87,17 +164,17 @@ namespace Xamflix.App.Forms
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
-            //if (_colapsed)
-            //{
-            //    await ScaleOutTitleImageAsync();
-            //}
-            //Play();
-            await Animate();
+            if(_collapsed)
+            {
+                await ScaleOutTitleImageAsync();
+            }
+
+            Play();
         }
 
         private void Play()
         {
-            var media = new MediaItem(Uri.EscapeUriString(@"https://xamflixdevgwcmedia-gewc1.streaming.media.azure.net/9f4ee191-59ff-48bf-bf53-2bfbec396d8b/The Midnight Sky  Final Trailer .ism/manifest(format=m3u8-aapl)"))
+            var media = new MediaItem("https://xamflixdeveuams-usea.streaming.media.azure.net/ba1b04de-ad6d-46f0-8b5f-a1d36e49f005/Outside%20the%20wire.ism/manifest(format=m3u8-aapl)")
                         {
                             MediaType = MediaType.Hls
                         };
