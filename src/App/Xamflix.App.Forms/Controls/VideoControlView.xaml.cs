dcb664cd23a6bf8d1c75d58c62b982fc9230b5d9
Gagik Kyurkchyan <gagik@xamlab.com>
2021-01-30 09:18:04 +0400

    Fixed popup animation.

---
+++ src/App/Xamflix.App.Forms/Controls/VideoControlView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MediaManager;
+using Xamarin.Forms.Xaml;
+using Xamflix.App.Forms.MarkupExtensions;
+
+namespace Xamflix.App.Forms.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoControlView
+    {
+        public event EventHandler<EventArgs> RepeatClicked; 
+
+        public VideoControlView()
+        {
+            InitializeComponent();
+        }
+
+        private void MuteButtonClicked(object sender, EventArgs e)
+        {
+            if (CrossMediaManager.Current.Volume.Muted)
+            {
+                CrossMediaManager.Current.Volume.Muted = false;
+                MuteButton.ImageSource = "unMute".GetImageSource();
+            }
+            else
+            {
+                CrossMediaManager.Current.Volume.Muted = true;
+                MuteButton.ImageSource = "mute".GetImageSource();
+            }
+        }
+
+        private void RepeatButtonClicked(object sender, EventArgs e)
+        {
+            RepeatClicked(sender, e);
+        }
+    }
+}
